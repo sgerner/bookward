a0ad4a64a85437cd323ad8c971e731e749594e86
Steven Gerner <sgerner@gmail.com>
2026-09-20 01:28:17 -0700

    fix: use CodeQL suppression for legacy hash

--- engine/afterword_engine/api_tokens.py
+++ engine/afterword_engine/api_tokens.py
@@ -40,7 +40,8 @@ def legacy_hash_api_token(token: str) -> str:
     ``hash_api_token`` immediately.
     """
 
-    return hashlib.sha256(  # lgtm[py/weak-sensitive-data-hashing]
+    # codeql[py/weak-sensitive-data-hashing]
+    return hashlib.sha256(
         token.encode("utf-8")
     ).hexdigest()
 
